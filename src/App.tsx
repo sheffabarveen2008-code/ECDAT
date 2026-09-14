e4d7c5b04cbc27ad64c9d924b9b3716d166f7899
sheffabarveen2008-code <sheffabarveen2008@gmail.com>
2026-09-14 20:08:36 +0530

    Add dynamic CBOM classification

--- src/App.tsx
+++ src/App.tsx
@@ -931,9 +931,24 @@ export default function App() {
                                 {item.risk}
                               </span>
                             </td>
-                             <td className="px-3 py-2 text-slate-300 font-mono text-[11px]">
-                              Crypto Asset
-                              </td>
+                            <td className="px-3 py-2 text-slate-300 font-mono text-[11px]">
+                              {item.algo?.toUpperCase().includes("RSA")||
+                                item.algo?.toUpperCase().includes("ECC")||
+                                item.algo?.toUpperCase().includes("ECDH")||
+                                item.algo?.toUpperCase().includes("ECDSA")||
+                                item.algo?.toUpperCase().includes("DH")||
+                                ? "Quantum Vulnerable"
+                              : item.algo?.toUpperCase().includes("ML-KEM")||
+                                item.algo?.toUpperCase().includes("ML-DSA")||
+                                item.algo?.toUpperCase().includes("SLH-DSA")||
+                                ? "PQC Asset"
+                               : item.algo?.toUpperCase().includes("AES")||
+                                ? "Symmetric Encryption"
+                               : item.algo?.toUpperCase().includes("SHA")||
+                                 item.algo?.toUpperCase().includes("MD5")||
+                                ? "Hashing Asset"
+                                : "Crypto Asset"}
+                            </td>
                             <td className="px-3 py-2 text-center">
                               <span
                               className={
